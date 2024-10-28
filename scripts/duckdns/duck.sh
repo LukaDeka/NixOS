7fbe1945e07072f5ce642ca0425566cb713952b9
luka <luka.dekanozishvili1@gmail.com>
2024-10-28 21:56:24 +0100

    updated duckdns script

--- scripts/duckdns/duck.sh
+++ scripts/duckdns/duck.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env
-echo url="https://www.duckdns.org/update?domains=lukadeka&token=6fb54327-8df2-4fca-b468-937598c1699b&ip=" | curl -k -o /home/luka/nixos/scripts/duckdns/duck.log -K - && date +'%d/%m/%y %H:%M' >> ./home/luka/nixos/scripts/duckdns/duck.txt
+
+TOKEN=$(cat ~/env/duckdns/token.txt)
+
+DOMAIN="lukadeka"
+DUCKDNS_URL="https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip="
+
+# Update DuckDNS IP and log the output
+curl -k -o /home/luka/nixos/scripts/duckdns/duck.log -K - <<< "url=${DUCKDNS_URL}"
+
+# Log date and time
+date +'%d/%m/%y %H:%M' >> /home/luka/nixos/scripts/duckdns/duck.txt
 
