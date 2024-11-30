e8f9d57ade75da52041e6b044580a030c990e4d6
luka <luka.dekanozishvili1@gmail.com>
2024-11-30 15:01:20 +0100

    restructured config

---
+++ hosts/berlin/zfs.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+
+  # The unique 32-bit ID of the machine. Used for making sure
+  # a ZFS pool isn't accidentally imported on a wrong machine.
+  # Generate it using: $ head -c 8 /etc/machine-id
+  networking.hostId = "462929b0";
+
+  # Set the name of the pool to import it on boot
+  boot.zfs.extraPools = [ "zfs0" ];
+}
+
