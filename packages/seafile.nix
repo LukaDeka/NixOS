bbe0f959ad10acea0649b17bfa5530e87977de09
luka <luka.dekanozishvili1@gmail.com>
2024-12-17 19:25:48 +0100

    added link expiration to seafile

--- packages/seafile.nix
+++ packages/seafile.nix
@@ -13,18 +13,41 @@ in
     initialAdminPassword = "mananakitrisad";
     ccnetSettings.General.SERVICE_URL = "https://seafile.${domain}";
 
-    # TODO: Figure out how to architect local access
+    # TODO: Figure out how to architect local access with proxy
     # seahubAddress = "127.0.0.1:39997";
-    seahubExtraConf = ''
+    seahubExtraConf =
+    let
+      password_min_len = "10";
+      password_unique_chars = "2";
+      expire_link_in_max_days = "14";
+      expire_link_in_default_days = "5";
+    in ''
       SRF_TRUSTED_ORIGINS = ["https://${domain}","${ip}"]
+
+      SITE_TITLE = 'Private Seafile'
+
+      # User password settings
+      USER_PASSWORD_MIN_LENGTH = ${password_min_len}
+      USER_PASSWORD_STRENGTH_LEVEL = ${password_unique_chars}
+      REPO_PASSWORD_MIN_LENGTH = ${password_min_len}
+
+      # User shared link settings
+      SHARE_LINK_FORCE_USE_PASSWORD = True
+      SHARE_LINK_PASSWORD_MIN_LENGTH = ${password_min_len}
+      SHARE_LINK_PASSWORD_STRENGTH_LEVEL = ${password_unique_chars}
+      SHARE_LINK_EXPIRE_DAYS_DEFAULT = ${expire_link_in_default_days}
+      SHARE_LINK_EXPIRE_DAYS_MAX = ${expire_link_in_max_days}
+      UPLOAD_LINK_EXPIRE_DAYS_DEFAULT = ${expire_link_in_default_days}
+      UPLOAD_LINK_EXPIRE_DAYS_MAX = ${expire_link_in_max_days}
+
     '';
 
     seafileSettings = {
       quota.default = "50"; # Amount of GB allotted to users
       history.keep_days = "3";
+      library_trash.expire_days = "3"; # Automatic cleanup of trash
 
       fileserver = {
-        # use_go_fileserver = true; # TODO: Diagnose why this option is broken
         host = "unix:/run/seafile/server.sock";
         web_token_expire_time = 18000; # Set max "upload time" to 5h
       };
