1bfdd20e0f2ae757d5e71356a28b36ca85f74030
luka <luka.dekanozishvili1@gmail.com>
2025-03-15 21:57:47 +0100

    added more git aliases

--- packages/aliases.nix
+++ packages/aliases.nix
@@ -9,13 +9,19 @@ in
     shellAliases = {
       ll = "ls -Ahlv --time-style=iso --group-directories-first";
       l  = "ls -hgov --time-style=iso --group-directories-first";
+
       gs = "git status";
       ga = "git add .";
+      gd = "git diff";
+      gp = "git push";
+      gr = "git restore";
+
       ".." = "cd ..";
+
       n = "cd ${homeDir}/nixos";
       f = "vim ${homeDir}/nixos/flake.nix";
       c = "vim ${homeDir}/nixos/hosts/${host}/configuration.nix";
-      s = "cd ${homeDir}/nixos && git add . && sudo nixos-rebuild switch --flake ${homeDir}/nixos";
+      s = "cd ${homeDir}/nixos && git add . && sudo nixos-rebuild switch --flake ${homeDir}/nixos; cd - &> /dev/null";
     };
     shellInit = ''
       export COLORTERM=truecolor;
