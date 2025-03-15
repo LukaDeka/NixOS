bb010910f75517889a30e0acde6c866e10159e78
luka <luka.dekanozishvili1@gmail.com>
2025-03-15 11:41:46 +0100

    fix: local DNS issue

--- packages/collabora-online.nix
+++ packages/collabora-online.nix
@@ -77,8 +77,14 @@ in
 
   # Edit /etc/hosts to force Collabora to resolve to localhost
   networking.hosts = {
-    "127.0.0.1" = ["nextcloud.${domain}" "collabora.${domain}"];
-    "::1" =       ["nextcloud.${domain}" "collabora.${domain}"];
+    "127.0.0.1" = [ "nextcloud.${domain}" "collabora.${domain}" ];
+    "::1" =       [ "nextcloud.${domain}" "collabora.${domain}" ];
   };
+
+  # Do not respond to DNS queries from /etc/hosts if Pi-hole is running
+  # TODO: test if this breaks anything without Pi-hole running
+  systemd.tmpfiles.rules = [
+    "f /var/lib/dnsmasq.d/no-hosts.conf 0644 root root - no-hosts"
+  ];
 }
 
