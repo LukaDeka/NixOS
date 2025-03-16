f233f2aec7de1113f8589c2e4f68ca69718c3566
luka <luka.dekanozishvili1@gmail.com>
2025-03-17 00:26:58 +0100

    rev-proxied jellyfin for mobile client

--- packages/jellyfin.nix
+++ packages/jellyfin.nix
@@ -1,11 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  domain = config.vars.domain;
+  ip = config.vars.ip;
+in
 {
   services.jellyfin = {
     enable = true;
     openFirewall = true;
     user = config.vars.username;
     # dataDir = "${config.vars.storageDir}/jellyfin";
+  };
+
+  services.nginx.virtualHosts = {
+    "jellyfin.${domain}" = {
+      sslCertificate = "/etc/env/ssl/${domain}.pem";
+      sslCertificateKey = "/etc/env/ssl/${domain}.key";
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://${ip}:8096";
+        proxyWebsockets = true;
+      };
+    };
   };
 }
 
