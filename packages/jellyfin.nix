f049e576fe59d1abce5e87d53dbadd6fa87113a8
luka <luka.dekanozishvili1@gmail.com>
2025-04-01 22:00:25 +0200

    added GPU hardware transcoding

--- packages/jellyfin.nix
+++ packages/jellyfin.nix
@@ -5,11 +5,23 @@ let
   ip = config.vars.ip;
 in
 {
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
     user = config.vars.username;
     # dataDir = "${config.vars.storageDir}/jellyfin";
+  };
+
+  users.users.jellyfin = {
+    isSystemUser = true;
+    group = "jellyfin";
+    extraGroups = [ "render" ];
   };
 
   services.nginx.virtualHosts = {
@@ -23,6 +35,28 @@ in
         proxyWebsockets = true;
       };
     };
+  };
+
+
+  # Enable vaapi on OS-level
+  # nixpkgs.config.packageOverrides = pkgs: {
+  #   vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  # };
+
+  boot.kernelParams = [
+    "i915.enable_guc=2"
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      vaapiVdpau
+      intel-compute-runtime-legacy1
+      # intel-compute-runtime
+      intel-media-sdk
+    ];
   };
 }
 
