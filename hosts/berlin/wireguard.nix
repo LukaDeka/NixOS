2bbe14c87b1cccb6696fcf1e57118f1f134a1552
luka <luka.dekanozishvili1@gmail.com>
2025-05-05 12:35:24 +0200

    fixed wireguard with br0

--- hosts/berlin/wireguard.nix
+++ hosts/berlin/wireguard.nix
@@ -12,7 +12,7 @@ in
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
-      address = [ "10.20.30.1/24" "192.168.255.0/16" ];
+      address = [ "10.20.20.10/24" ];
 
       listenPort = 39999;
 
@@ -21,28 +21,28 @@ in
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       postUp = ''
         ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.20.30.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.20.20.0/24 -o br0 -j MASQUERADE
         ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o br0 -j MASQUERADE
       '';
 
       # Undo the above
       preDown = ''
         ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.20.30.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.20.20.0/24 -o br0 -j MASQUERADE
         ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o br0 -j MASQUERADE
       '';
 
       peers = [
         { # LG Gram
           publicKey = "sSi8ZrKc99rlqdXJY6iuQC1QyXe+dKJOHZdq4CL4H0k=";
           presharedKeyFile = "/etc/env/wireguard/psk";
-          allowedIPs = [ "10.20.30.2/32" "192.168.255.1/32" ];
+          allowedIPs = [ "10.20.20.20/32" ];
         }
         { # Moto G30
           publicKey = "vBlpzpyMa170OQL97Zj0bWCJwV0azpSGBnPVUIsJy1Y=";
-          allowedIPs = [ "10.20.30.3/32" ];
+          allowedIPs = [ "10.20.20.30/32" ];
         }
       ];
     };
