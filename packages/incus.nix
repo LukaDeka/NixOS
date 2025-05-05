837a005fe6f1f1051f645b6b5b3986fbc9af0670
luka <luka.dekanozishvili1@gmail.com>
2025-05-05 22:54:04 +0200

    fixed dynamic MAC isse

--- packages/incus.nix
+++ packages/incus.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }:
 
+let
+  ethernetMAC = config.vars.ethernetMAC;
+in
 {
   networking = {
     # Enable a network bridge and route all traffic through it, since
     # the services running on the VMs are inaccessible otherwise.
     # With this setup, each VM gets an IP address through DHCP
     interfaces = {
-      br0.useDHCP = true;
+      br0.macAddress = ethernetMAC; # To make sure the MAC never changes
+      br0.useDHCP = true; # Make sure you set a static IP in your router
       eth0.useDHCP = false;
       wlan0.useDHCP = false; # Disable Wi-Fi, since it interferes with br0
     };
