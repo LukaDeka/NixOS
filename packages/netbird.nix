e418ad42c79d122e4bb37cb263f301d64ee1d963
luka <luka.dekanozishvili1@gmail.com>
2025-07-14 18:00:18 +0200

    added relay server

--- packages/netbird.nix
+++ packages/netbird.nix
@@ -6,6 +6,7 @@ let
   netbirdDomain = "netbird.${domain}";
 in
 {
+  services.netbird.enable = true; # Enable the client as well
   services.netbird.server = {
     enable = true;
     enableNginx = true;
@@ -37,6 +38,9 @@ in
     management = {
       enable = true;
       enableNginx = true;
+      domain = netbirdDomain;
+      turnDomain = netbirdDomain;
+      singleAccountModeDomain = netbirdDomain;
       oidcConfigEndpoint = "https://auth.${domain}/.well-known/openid-configuration";
       # For Authentik:
       # oidcConfigEndpoint = "https://auth.${domain}/application/o/netbird/.well-known/openid-configuration";
@@ -55,7 +59,24 @@ in
           ClientID = clientId;
         };
 
-        TURNConfig.Secret._secret = "/etc/env/netbird/turn_password";
+        TURNConfig = {
+          Secret._secret = "/etc/env/netbird/turn_password";
+          CredentialsTTL = "12h";
+          TimeBasedCredentials = false;
+          Turns = [
+            {
+              Password._secret = "/etc/env/netbird/turn_password";
+              Proto = "udp";
+              URI = "turn:${netbirdDomain}:3478";
+              Username = "netbird";
+            }
+          ];
+        };
+        Relay = {
+          Addresses = [ "rel://${netbirdDomain}:33080" ];
+          CredentialsTTL = "24h";
+          Secret._secret = "/etc/env/netbird/relay_secret";
+        };
         DataStoreEncryptionKey._secret = "/etc/env/netbird/data_store_encryption_key";
       };
     };
@@ -75,7 +96,23 @@ in
     }
   ];
 
-  networking.firewall.allowedTCPPorts = [ 80 443 3478 10000 ];
-  networking.firewall.allowedUDPPorts = [ 3478 10000 ];
+  virtualisation.oci-containers.containers.netbird-relay = {
+    image = "netbirdio/relay:latest";
+    ports = [
+      "33080:33080"
+    ];
+    environment = {
+      NB_LOG_LEVEL = "info";
+      NB_LISTEN_ADDRESS = ":33080";
+      NB_EXPOSED_ADDRESS = "${netbirdDomain}:33080";
+    };
+    environmentFiles = [
+      "/etc/env/netbird/relay_secret_container"
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 3478 10000 33080 ];
+  networking.firewall.allowedUDPPorts = [ 3478 5349 33080 ];
+  networking.firewall.allowedUDPPortRanges = [{ from = 40000; to = 40050; }]; # TURN
 }
 
